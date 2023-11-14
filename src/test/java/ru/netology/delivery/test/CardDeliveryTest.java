a8c393c9150ff100efd86f45a4fb7ebbc7894c03
Demidoff Serg <serega.dem@inbox.ru>
2023-11-14 11:20:02 +0800

    add changes

--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -36,9 +36,9 @@ class DeliveryTest {
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
-        $(byText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на "  + firstMeetingDate))
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
@@ -48,7 +48,7 @@ class DeliveryTest {
                 .shouldBe(visible);
         $("[data-test-id=replan-notification] button").click();
         $("[data-test-id=success-notification] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + secondMeetingDate))
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
     }
 }
